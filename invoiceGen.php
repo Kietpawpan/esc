753fd3f0b857f030c772ba2d232205fe120c3ff4
jTech <kietpawpan@gmail.com>
2024-03-03 11:01:50 +0700

    Update invoiceGen.php

--- invoiceGen.php
+++ invoiceGen.php
@@ -151,20 +151,6 @@
 </div>
 
 <br>
-<!HIDE PIN CODE>
-	<script type='text/javascript'>
-        	const togglePassword = document.querySelector("#togglePassword");
-	        const password = document.querySelector("#input");
-        	togglePassword.addEventListener("click", function () {
-	            const type = password.getAttribute("type") === "password" ? "text" : "password";
-        	    password.setAttribute("type", type);
-                    this.classList.toggle("bi-eye");
-	        });
-    		const form = document.querySelector("form");
-        	form.addEventListener('submit', function (e) {
-	        e.preventDefault();
-	        });
-    	</script>
 
 
 </body>
